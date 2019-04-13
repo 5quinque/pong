473d30676d5283c04eb05c15c23002894f57b4a5
Ryan Linnit <ryan@ryanl.co.uk>
2019-04-13 07:22:42 +0000

    all rubbish

--- pt.c
+++ pt.c
@@ -1,3 +1,4 @@
+/* ℛℒ ⅯⅯⅪⅩ*/
 #include <stdio.h>
 #include <time.h>
 #include <string.h> // strcmp
@@ -6,6 +7,7 @@
 void JKF(int Clk, int J, int K, int *Q, int *NQ);
 void SRF(int NS, int NR, int *Q, int *NQ);
 int ATI(char *action);
+void debug(size_t n, int* input, char* action, char* comment);
 
 // Lawn Tennis
 // http://www.pong-story.com/LAWN_TENNIS.pdf
@@ -46,27 +48,62 @@ int ATI(char *action);
  *   |_________________________|
  *
  */
-void JKF(int Clk, int J, int K, int *Q, int *NQ) {
+void JKF2(int Clk, int J, int K, int *Q, int *NQ) {
   int NSA = 0, NRA = 0;
   int NSB = 0, NRB = 0;
 
   int NP3 = 0, NNP3 = 0;
 
+  int input[] = {Clk, J, *NQ};
+  size_t n = sizeof(input)/sizeof(input[0]);
+
+  debug(n, input, "NAND", "NSA");
   NSA = !((Clk & J) & *NQ);
+
+  input[0] = Clk;
+  input[1] = K;
+  input[2] = *Q;
+  debug(n, input, "NAND", "NRA");
+  NRA = !((Clk & K) & *Q);
+
+  SRF(NSA, NRA, &NP3, &NNP3);
+
+  input[0] = NP3;
+  input[1] = Clk;
+  debug(2, input, "NAND", "NSB");
+  NSB = !(NP3 & (!Clk));
+
+  input[0] = NNP3;
+  input[1] = Clk;
+  debug(2, input, "NAND", "NRB");
+  NRB = !(NNP3 & (!Clk));
+
+  SRF(NSB, NRB, Q, NQ);
+
+  input[0] = Clk;
+  input[1] = J;
+  input[2] = *NQ;
+  debug(n, input, "NAND", "NSA");
+  NSA = !((Clk & J) & *NQ);
+
+  input[0] = Clk;
+  input[1] = K;
+  input[2] = *Q;
+  debug(n, input, "NAND", "NRA");
   NRA = !((Clk & K) & *Q);
   SRF(NSA, NRA, &NP3, &NNP3);
 
+  input[0] = NP3;
+  input[1] = !Clk;
+  debug(2, input, "NAND", "NSB");
   NSB = !(NP3 & (!Clk));
-  NRB = !(NNP3 & (!Clk));
-  SRF(NSB, NRB, Q, NQ);
-  
-  NSA = !((Clk & J) & *NQ);
-  NRA = !((Clk & K) & *Q);
-  SRF(NSA, NRA, &NP3, &NNP3);
 
-  NSB = !(NP3 & (!Clk));
+  input[0] = NNP3;
+  input[1] = !Clk;
+  debug(2, input, "NAND", "NRB");
   NRB = !(NNP3 & (!Clk));
   SRF(NSB, NRB, Q, NQ);
+  printf("asd");
 
   //printf("NS %d NR %d\n", NS, NR);
 
@@ -80,24 +117,25 @@ void JKF(int Clk, int J, int K, int *Q, int *NQ) {
   //SRF(NS, NR, Q, NQ);
 
 }
-/*
-void JKF(int Clk, int J, int K, int *Q, int *NQ) {
-  if(Clk&J&(!K)) {
-    *Q = 1;
-    *NQ = 0;
-  }
-  
-  if(Clk&K&(!J)) {
-    *Q = 0;
-    *NQ = 1;
-  }
 
-  if(Clk&J&K) {
-    *Q = *NQ;
+void JKF(int Clk, int J, int K, int *Q, int *NQ) {
+    if (Clk) {
+        return;
+    }
+    // Reset Q = 0
+    if (J == 0 && K == 1) {
+        *Q = 0;
+    }
+    // Set Q = 1
+    if (J == 1 && K == 0) {
+        *Q = 1;
+    }
+    // Toggle
+    if (J == 1 && K == 1) {
+        *Q = !*Q;
+    }
     *NQ = !*Q;
-  }
 }
-*/
 
 /* SR Flip-Flop
  *      ____
@@ -123,35 +161,60 @@ void JKF(int Clk, int J, int K, int *Q, int *NQ) {
  *
  */
 void SRF(int NS, int NR, int *Q, int *NQ) {
+  int input[] = {NS, *NQ};
+  size_t n = sizeof(input)/sizeof(input[0]);
+
+  debug(n, input, "NAND", "Q");
   *Q = !(NS & *NQ);
+
+  input[0] = NR;
+  input[1] = *Q;
+  debug(n, input, "NAND", "NQ");
   *NQ = !(NR & *Q);
+
+  input[0] = NS;
+  input[1] = *NQ;
+  debug(n, input, "NAND", "Q");
   *Q = !(NS & *NQ);
+
 }
 
 void FBRC(
         int Clk,
-        int *Q1,
-        int *NQ1,
-        int *Q2,
-        int *NQ2,
-        int *Q3,
-        int *NQ3,
-        int *Q4,
-        int *NQ4
+        int *QA,
+        int *NQA,
+        int *QB,
+        int *NQB,
+        int *QC,
+        int *NQC,
+        int *QD,
+        int *NQD
         ) {
-  JKF(Clk, 1, 1, Q1, NQ1);
-  JKF(*Q1, 1, 1, Q2, NQ2);
-  JKF(*Q2, 1, 1, Q3, NQ3);
-  JKF(*Q3, 1, 1, Q4, NQ4);
-  printf("%d %d%d%d%d\n", Clk, *Q4, *Q3, *Q2, *Q1);
+  JKF(Clk, 1, 1, QA, NQA);
+  JKF(*QA, 1, 1, QB, NQB);
+  JKF(*QB, 1, 1, QC, NQC);
+  JKF(*QC, 1, 1, QD, NQD);
+  printf("%d %d%d%d%d\n", Clk, *QA, *QB, *QC, *QD);
   //printf("%d%d%d%d\n", Clk, *NQ4, *NQ3, *NQ2, *NQ1);
   //printf("%d%d\n", *Q2, *Q1);
 }
 
-void debug(size_t n, int* input, char* action) {
+void debug(size_t n, int* input, char* action, char* comment) {
   int tmp = 0;
 
+
+  printf("-▷|-\n"); // NOT
+
+  printf("0 ──┌────┐\n");
+  printf("    │ && │∘- 1\n");
+  printf("0 ──└────┘\n");
+
   for (unsigned int i = 0; i < n; i++) {
+    printf(" %d - - \n", input[i]);
+  }
+
+  for (unsigned int i = 0; i < n; i++) {
+    continue;
     if (i == n-1) {
       printf("%d ", input[i]);
     } else {
@@ -183,14 +246,16 @@ void debug(size_t n, int* input, char* action) {
     case 1:
     case 3:
     case 5:
-      printf(" = %d\n", tmp);
+      printf(" = %d", tmp);
       break;
     case 2:
     case 4:
     case 6:
-      printf(" = %d\n", !tmp);
+      printf(" = %d", !tmp);
       break;
   }
+
+  printf(" - %s\n", comment);
 }
 
 int ATI(char* action) {
@@ -212,7 +277,7 @@ int ATI(char* action) {
 
 int main() {
   struct timespec ts = { .tv_sec = 0, .tv_nsec = 100000000L };
-  int cycles = 9;
+  int cycles = 10;
   int Clk = 0;
 
   int Q = 0, NQ = 0;
@@ -221,27 +286,27 @@ int main() {
   int QC = 0, NQC = 0;
   int QD = 0, NQD = 0;
 
-  int input[] = {1, 1, 1};
-  size_t n = sizeof(input)/sizeof(input[0]);
-  debug(n, input, "XOR");
-
+  //SRF(1, 0, &Q, &NQ); // 1 0 0 1
+    
   for (int i = 0; i < cycles; i++) {
     Clk ^= 1;
     
-    /*
+    //JKF(Clk, 0, 0, &Q, &NQ);
+    //printf("%d\t%d %d\n", Clk, Q, NQ);
+    
+    /* */
     FBRC(
             Clk,
-            &Q1,
-            &NQ1,
-            &Q2,
-            &NQ2,
-            &Q3,
-            &NQ3,
-            &Q4,
-            &NQ4
+            &QA,
+            &NQA,
+            &QB,
+            &NQB,
+            &QC,
+            &NQC,
+            &QD,
+            &NQD
             );
-            */
-
+    /* */
             
     //SRF(1, 0, &Q, &NQ); // 1 0 0 1
     //printf("%d %d %d %d\n", 1, 0, Q, NQ);
