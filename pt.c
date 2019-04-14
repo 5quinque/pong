7116a6a049f421687f5481cc4e44c3e2e71982d8
Ryan Linnit <ryan@ryanl.co.uk>
2019-04-14 19:07:17 +0000

    Reducing if statements

--- pt.c
+++ pt.c
@@ -1,4 +1,4 @@
-/* ℛℒ ⅯⅯⅪⅩ*/
+/* ℛℒ ⅯⅯⅪⅩ */
 #include <stdio.h>
 #include <time.h>
 
@@ -8,28 +8,50 @@ struct CQNQ {
     int NQ;
 };
 
-//void FBRC(int Clk, int *Count);
 void FBRC(int Clk, struct CQNQ *A, struct CQNQ *B, struct CQNQ *C, struct CQNQ *D);
 void JKF(int Clk, int J, int K, struct CQNQ *JKF);
 
 void JKF(int Clk, int J, int K, struct CQNQ *JKF) {
     if ((Clk != JKF->Clk) && !Clk) {
         // Reset Q = 0
-        if (J == 0 && K == 1) {
-            JKF->Q = 0;
-        }
+        //if (J == 0 && K == 1) {
+        //    JKF->Q = 0;
+        //}
         // Set Q = 1
-        if (J == 1 && K == 0) {
-            JKF->Q = 1;
-        }
+        //if (J == 1 && K == 0) {
+        //    JKF->Q = 1;
+        //}
         // Toggle
-        if (J == 1 && K == 1) {
-            JKF->Q = !JKF->Q;
-        }
+        //if (J == 1 && K == 1) {
+        //    JKF->Q = !JKF->Q;
+        //}
+        JKF->Q = (J && !K) || // Reset Q = 0
+            ((J && K) && JKF->NQ) || // Toggle
+            (!JKF->Q && !JKF->NQ); // Set Q = 1
         JKF->NQ = !JKF->Q;
     }
+
     JKF->Clk = Clk;
 }
+
+/*
+     7493
+ ┌──────────┐
+━│B        A│━
+ │          │
+━│R1      NC│━
+ │          │
+━│R2      QA│━
+ │          │
+━│NC      QD│━
+ │          │
+━│VCC    GND│━
+ │          │
+━│NC      QB│━
+ │          │
+━│NC      QC│━
+ └──────────┘
+*/
 
 void FBRC(int Clk, struct CQNQ *A, struct CQNQ *B, struct CQNQ *C, struct CQNQ *D) {
   JKF(Clk, 1, 1, A);
@@ -39,8 +61,13 @@ void FBRC(int Clk, struct CQNQ *A, struct CQNQ *B, struct CQNQ *C, struct CQNQ *
 }
 
 int main() {
-  struct timespec ts = { .tv_sec = 0, .tv_nsec = 100000000L };
-  int cycles = 70;
+  //struct timespec ts = { .tv_sec = 0, .tv_nsec = 100000000L };
+  //
+  // 1 second   / 7.1575MHz
+  // 1000000000 / 7157500 = 139.713587146
+  //struct timespec ts = { .tv_sec = 0, .tv_nsec = 139.713587146 };
+  struct timespec ts = { .tv_sec = 0, .tv_nsec = 10000L };
+  int cycles = 64;
   int Clk = 0;
 
   struct CQNQ A = { .Clk = 0, .Q = 0, .NQ = 0 };
@@ -52,13 +79,10 @@ int main() {
 
   for (int i = 0; i < cycles; i++) {
     Clk ^= 1;
-    
-    count = A.Q;
-    count |= B.Q << 1;
-    count |= C.Q << 2;
-    count |= D.Q << 3;
-    if (Clk) {
-        printf("%d\t%d\t%d%d%d%d\n", Clk, count, D.Q, C.Q, B.Q, A.Q);
+
+    count = A.Q | ((B.Q << 1) | (C.Q << 2) | (D.Q << 3));
+    if (!Clk) {
+      printf("%d\t%d\t%d%d%d%d\n", Clk, count, D.Q, C.Q, B.Q, A.Q);
     }
     
     FBRC(Clk, &A, &B, &C, &D);
